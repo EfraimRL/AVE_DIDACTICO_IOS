a4b8b6b179b53a7f4867c2b3acf7de1cae584f30
Bryancm <bryan.mtzs@gmail.com>
2018-03-07 18:44:10 -0600

    Initial Commit

---
+++ AVE_DIDACTICO_IOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AVE_DIDACTICO_IOS
+//
+//  Created by brauliop on 3/7/18.
+//  Copyright © 2018 BryanMtz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
